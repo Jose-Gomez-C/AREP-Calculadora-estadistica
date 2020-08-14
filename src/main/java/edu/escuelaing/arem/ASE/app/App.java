12f6c985a676772365c844f59b680f5e99e3fe97
Jose Luis Gomez Camacho <josemania10@gmail.com>
2020-08-13 23:03:21 -0500

    Generacion del javadoc

--- src/main/java/edu/escuelaing/arem/ASE/app/App.java
+++ src/main/java/edu/escuelaing/arem/ASE/app/App.java
@@ -1,6 +1,9 @@
 package edu.escuelaing.arem.ASE.app;
 
-public class app{
+public class App{
+	/**
+	 * @author Jose Luis Gomez Camacho
+	 */
 	public static void main(String[] args){ 
 		Calculadora calculos= new Calculadora("datos.txt");
 		try {
